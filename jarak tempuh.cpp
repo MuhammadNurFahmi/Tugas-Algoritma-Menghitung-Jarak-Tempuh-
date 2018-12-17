0851b3b170a87b6803167f8ad3733779bb59ab74
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-17 09:49:39 +0700

    Add files via upload

---
+++ jarak tempuh.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int v,t;
+	
+	cout << "Masukkan Kecepatan : ";
+	cin >> v;
+	
+	cout << "Masukkan Waktu : ";
+	cin >> t;
+	
+	cout << "Jarak Tempuh ( S ) = " << v*t ;
+	
+	return 0;
+}
